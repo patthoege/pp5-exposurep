1032404a5c0acdf32448b606cfb92446024495af
Patt Höge <paty.hoge98@gmail.com>
2024-02-20 20:06:16 +0000

    add handle change of data and handle function to upload and change image from the create post form

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -12,37 +12,68 @@ import styles from "../../styles/PostCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import Asset from "../../components/Assets";
+import { Image } from "react-bootstrap";
+
 
 function PostCreateForm() {
-
   const [errors, setErrors] = useState({});
+
+  const [postData, setPostData] = useState({
+    title: "",
+    location: "",
+    content: "",
+    image: "",
+  });
+  const { title, location, content, image } = postData;
+
+  const handleChange = (event) => {
+    setPostData({
+      ...postData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleChangeImage = (event) => {
+    if (event.target.files.length) {
+      URL.revokeObjectURL(image);
+      setPostData({
+        ...postData,
+        image: URL.createObjectURL(event.target.files[0]),
+      });
+    }
+  };
 
 
   const textFields = (
     <div className="text-center">
-    
-    <Form.Group>
-        <Form.Label>Title</Form.Label>
-        <Form.Control
-          type="text"
-          name="title"
-        />
-    </Form.Group>
-    <Form.Group>
-        <Form.Label>Location</Form.Label>
-        <Form.Control
-          type="text"
-          name="location"
-        />
-    </Form.Group>
-    <Form.Group>
-        <Form.Label>Content</Form.Label>
-        <Form.Control
-          as="textarea"
-          rows={5}
-          name="content"
-        />
-    </Form.Group>
+        <Form.Group>
+            <Form.Label>Title</Form.Label>
+            <Form.Control
+            type="text"
+            name="title"
+            value={title}
+            onChange={handleChange}
+            />
+        </Form.Group>
+        <Form.Group>
+            <Form.Label>Location</Form.Label>
+            <Form.Control
+            type="text"
+            name="location"
+            value={location}
+            onChange={handleChange}
+            />
+        </Form.Group>
+        <Form.Group>
+            <Form.Label>Content</Form.Label>
+            <Form.Control
+            as="textarea"
+            rows={5}
+            name="content"
+            value={content}
+            onChange={handleChange}
+            />
+        </Form.Group>
     
       <Button
         className={`${btnStyles.Button} ${btnStyles.Blue}`}
@@ -64,16 +95,38 @@ function PostCreateForm() {
             className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
           >
             <Form.Group className="text-center">
-              
+            {image ? (
+                <>
+                  <figure>
+                    <Image className={appStyles.Image} src={image} rounded />
+                  </figure>
+                  <div>
+                    <Form.Label
+                      className={`${btnStyles.Button} ${btnStyles.Blue} btn`}
+                      htmlFor="image-upload"
+                    >
+                      Change the image
+                    </Form.Label>
+                  </div>
+                </>
+              ) : (
                 <Form.Label
                   className="d-flex justify-content-center"
                   htmlFor="image-upload"
                 >
-                <Asset 
-                   src={Upload}
-                   message="Click or tap to upload an image"
-                />
+                  <Asset
+                    src={Upload}
+                    message="Click or tap to upload an image"
+                  />
                 </Form.Label>
+              )}
+
+
+                <Form.File
+                    id="image-upload"
+                    accept="image/*"
+                    onChange={handleChangeImage}
+                />
 
             </Form.Group>
             <div className="d-md-none">{textFields}</div>
