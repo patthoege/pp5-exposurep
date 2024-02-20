f04975251a50610d4d7e62a815adb71314109c74
Patt Höge <paty.hoge98@gmail.com>
2024-02-20 18:56:31 +0000

    added toggle hook component and added to the NavBar Toggler

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,10 +6,13 @@ import { NavLink } from "react-router-dom";
 import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
 import Avatar from "./Avatar";
 import axios from "axios";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
+
+  const { expanded, setExpanded, ref } = useClickOutsideToggle();
 
   const handleSignOut = async () => {
     try {
@@ -72,7 +75,7 @@ const NavBar = () => {
   );
 
   return (
-    <Navbar className={styles.NavBar} variant="dark" expand="md" fixed="top">
+    <Navbar expanded={expanded} className={styles.NavBar} variant="dark" expand="md" fixed="top">
       <Container>
       <NavLink to="/">
           <Navbar.Brand>
@@ -80,7 +83,10 @@ const NavBar = () => {
           </Navbar.Brand>
       </NavLink>
       {currentUser && addPostIcon}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+        ref={ref}
+        onClick={() => setExpanded(!expanded)}
+        aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <NavLink 
