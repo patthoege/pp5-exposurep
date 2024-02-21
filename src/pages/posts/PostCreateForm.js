e70de6174ea76e2e369147d10833617415415fb9
Patt Höge <paty.hoge98@gmail.com>
2024-02-21 09:06:35 +0000

    add error handlers to each input field of the post form

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -5,6 +5,7 @@ import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import Alert from "react-bootstrap/Alert";
 
 import Upload from "../../assets/upload.png";
 
@@ -79,6 +80,11 @@ function PostCreateForm() {
             onChange={handleChange}
             />
         </Form.Group>
+        {errors?.title?.map((message, idx) => (
+          <Alert variant="warning" key={idx}>
+            {message}
+          </Alert>
+        ))}
         <Form.Group>
             <Form.Label>Location</Form.Label>
             <Form.Control
@@ -88,6 +94,11 @@ function PostCreateForm() {
             onChange={handleChange}
             />
         </Form.Group>
+        {errors?.location?.map((message, idx) => (
+          <Alert variant="warning" key={idx}>
+            {message}
+          </Alert>
+        ))}
         <Form.Group>
             <Form.Label>Content</Form.Label>
             <Form.Control
@@ -98,6 +109,11 @@ function PostCreateForm() {
             onChange={handleChange}
             />
         </Form.Group>
+        {errors?.content?.map((message, idx) => (
+          <Alert variant="warning" key={idx}>
+            {message}
+          </Alert>
+        ))}
     
       <Button
         className={`${btnStyles.Button} ${btnStyles.Blue}`}
@@ -151,6 +167,12 @@ function PostCreateForm() {
                     ref={imageInput}
                 />
             </Form.Group>
+            {errors?.image?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+
             <div className="d-md-none">{textFields}</div>
           </Container>
         </Col>
