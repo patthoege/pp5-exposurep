ed33ce6e9ae3015811bea499543e7b1adb71854a
Patt Höge <paty.hoge98@gmail.com>
2024-02-26 14:21:23 +0000

    added handle edit and delete function to posts

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
 import { axiosRes } from "../../api/axiosDefaults";
 import { MoreDropdown } from "../../components/MoreDropdown";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const Post = (props) => {
   const {
@@ -27,6 +28,20 @@ const Post = (props) => {
 
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
+  const history = useHistory();
+
+  const handleEdit = () => {
+    history.push(`/posts/${id}/edit`);
+  };
+
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`/posts/${id}`);
+      history.goBack();
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const handleLike = async () => {
     try {
@@ -74,7 +89,12 @@ const Post = (props) => {
                 <i className="fas fa-map-marker-alt"></i> 
                 {location}
             </div>
-            {is_owner && postPage && <MoreDropdown />}
+            {is_owner && postPage && (
+              <MoreDropdown
+                handleEdit={handleEdit}
+                handleDelete={handleDelete}
+              />
+            )}
           </div>
         </Media>
       </Card.Body>
