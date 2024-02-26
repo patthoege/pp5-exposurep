225f69b947e3fcd2181b7f6f9403ed1e9fcd3c7c
Patt Höge <paty.hoge98@gmail.com>
2024-02-26 19:57:18 +0000

    Added comments component and supporting module CSS and populate comments

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -9,8 +9,10 @@ import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
 
+
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import Comment from "../comments/Comments";
 
 function PostPage() {
   const { id } = useParams();
@@ -23,11 +25,12 @@ function PostPage() {
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: post }] = await Promise.all([
+        const [{ data: post }, { data: comments }] = await Promise.all([
           axiosReq.get(`/posts/${id}`),
+          axiosReq.get(`/comments/?post=${id}`),
         ]);
         setPost({ results: [post] });
-        console.log(post);
+        setComments(comments);
       } catch (err) {
         console.log(err);
       }
@@ -53,6 +56,17 @@ function PostPage() {
           ) : comments.results.length ? (
             "Comments"
           ) : null}
+
+          {comments.results.length ? (
+            comments.results.map((comment) => (
+              <Comment key={comment.id} {...comment} />
+            ))
+          ) : currentUser ? (
+            <span>No comments yet, be the first to comment!</span>
+          ) : (
+            <span>No comments... yet</span>
+          )}
+
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
