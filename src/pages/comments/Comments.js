b469391a247d7c0fc33921acc03935b275322b1d
Patt Höge <paty.hoge98@gmail.com>
2024-02-27 19:07:10 +0000

    added dropdown menu to comments component

--- src/pages/comments/Comments.js
+++ src/pages/comments/Comments.js
@@ -2,10 +2,15 @@ import React from "react";
 import { Media } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { MoreDropdown } from "../../components/MoreDropdown";
 import styles from "../../styles/Comment.module.css";
 
 const Comment = (props) => {
   const { profile_id, profile_image, owner, updated_at, content } = props;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
 
   return (
     <div>
@@ -19,6 +24,9 @@ const Comment = (props) => {
           <span className={styles.Date}>{updated_at}</span>
           <p>{content}</p>
         </Media.Body>
+        {is_owner && (
+          <MoreDropdown  />
+        )}
       </Media>
     </div>
   );
