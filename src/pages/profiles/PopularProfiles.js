d2dc6513e370ebc09f3f8874cc754ab661dac413
Patt Höge <paty.hoge98@gmail.com>
2024-02-29 06:32:40 +0000

    Added PopularProfiles mobile to PostsPage and customize it for smaller screens

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -5,7 +5,7 @@ import { axiosReq } from "../../api/axiosDefaults";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import Asset from '../../components/Assets';
 
-const PopularProfiles = () => {
+const PopularProfiles = ({ mobile }) => {
 
   const [profileData, setProfileData] = useState({
     // reminder: store data for the pageProfile later
@@ -35,13 +35,25 @@ const PopularProfiles = () => {
   }, [currentUser]);
 
   return (
-    <Container className={appStyles.Content}> 
+    <Container 
+        className={`${appStyles.Content} ${
+            mobile && "d-lg-none text-center mb-3"
+        }`}
+    > 
         {popularProfiles.results.length ? (
         <>
             <p>Most followed profiles</p>
-            {popularProfiles.results.map(profile => (
-            <p key={profile.id}>{profile.owner}</p>
-            ))}
+            {mobile ? (
+                <div className="d-flex justify-content-around">
+                    {popularProfiles.results.slice(0,4).map(profile => (
+                    <p key={profile.id}>{profile.owner}</p>
+                    ))}
+                </div>
+            ) : (
+                popularProfiles.results.map(profile => (
+                <p key={profile.id}>{profile.owner}</p>
+                ))
+            )}
         </>
         ) : (
             <Asset spinner />
