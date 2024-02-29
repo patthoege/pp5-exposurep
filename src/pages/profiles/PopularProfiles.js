47990575aeba56827c2e1a3b27ba788fb01d5a8f
Patt Höge <paty.hoge98@gmail.com>
2024-02-29 05:29:49 +0000

    Created PopularProfiles and import it to PostsPage

---
+++ src/pages/profiles/PopularProfiles.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import appStyles from "../../App.module.css";
+import { Container } from 'react-bootstrap';
+
+const PopularProfiles = () => {
+  return (
+    <Container className={appStyles.Content}> 
+    <p>Most followed profiles</p>
+
+    </Container> 
+  )
+}
+
+export default PopularProfiles
