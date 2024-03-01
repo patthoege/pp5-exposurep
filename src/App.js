b7a98261d160bdd7843e40637fe9340253a4a611
Patt Höge <paty.hoge98@gmail.com>
2024-03-01 16:32:44 +0000

    added CreateEventForm file and import into App

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import ProfilePage from "./pages/profiles/ProfilePage";
 import UsernameForm from "./pages/profiles/UsernameForm";
 import UserPasswordForm from "./pages/profiles/UserPasswordForm";
 import ProfileEditForm from "./pages/profiles/ProfileEditForm";
+import CreateEventForm from "./pages/events/CreateEventForm";
 
 
 function App() {
@@ -61,7 +62,7 @@ function App() {
             <Route exact path="/profiles/:id/edit/username" render={() => <UsernameForm />} />
             <Route exact path="/profiles/:id/edit/password" render={() => <UserPasswordForm />} />
             <Route exact path="/profiles/:id/edit" render={() => <ProfileEditForm />} />
-            <Route exact path="/events/create" render={() => <h1>Create Events</h1>} />
+            <Route exact path="/events/create" render={() => <CreateEventForm />} />
             <Route render={() => <p>Page Not Found!</p>} />
           </Switch>
         </Container>
---
+++ src/pages/events/CreateEventForm.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function CreateEventForm(){
+
+    return (
+        <h1>Create Event Form!</h1>
+    );
+};
+
+export default CreateEventForm;
