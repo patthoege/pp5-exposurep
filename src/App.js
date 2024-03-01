91b154464d540e524503fdccd79ee13943b979d5
Patt Höge <paty.hoge98@gmail.com>
2024-03-01 10:37:12 +0000

    created user password form and import it into App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import { useCurrentUser } from "./contexts/CurrentUserContext";
 import PostEditForm from "./pages/posts/PostEditForm";
 import ProfilePage from "./pages/profiles/ProfilePage";
 import UsernameForm from "./pages/profiles/UsernameForm";
+import UserPasswordForm from "./pages/profiles/UserPasswordForm";
 
 
 function App() {
@@ -57,6 +58,7 @@ function App() {
             <Route exact path="/posts/:id" render={() => <PostPage /> } />
             <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
             <Route exact path="/profiles/:id/edit/username" render={() => <UsernameForm />} />
+            <Route exact path="/profiles/:id/edit/password" render={() => <UserPasswordForm />} />
             <Route render={() => <p>Page Not Found!</p>} />
           </Switch>
         </Container>
