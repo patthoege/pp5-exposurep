c3b06d04b9e22958b8f8b02dd16955ec2bed88e8
Patt Höge <paty.hoge98@gmail.com>
2024-03-01 10:39:12 +0000

    created profile edit form and import it into App.js

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import PostEditForm from "./pages/posts/PostEditForm";
 import ProfilePage from "./pages/profiles/ProfilePage";
 import UsernameForm from "./pages/profiles/UsernameForm";
 import UserPasswordForm from "./pages/profiles/UserPasswordForm";
+import ProfileEditForm from "./pages/profiles/ProfileEditForm";
 
 
 function App() {
@@ -59,6 +60,7 @@ function App() {
             <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
             <Route exact path="/profiles/:id/edit/username" render={() => <UsernameForm />} />
             <Route exact path="/profiles/:id/edit/password" render={() => <UserPasswordForm />} />
+            <Route exact path="/profiles/:id/edit" render={() => <ProfileEditForm />} />
             <Route render={() => <p>Page Not Found!</p>} />
           </Switch>
         </Container>
