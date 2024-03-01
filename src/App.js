6e1de6f67ca9440fe7866b8d5ef3e31828e300a0
Patt Höge <paty.hoge98@gmail.com>
2024-03-01 10:34:19 +0000

    created username form and import it into App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import PostsPage from "./pages/posts/PostsPage";
 import { useCurrentUser } from "./contexts/CurrentUserContext";
 import PostEditForm from "./pages/posts/PostEditForm";
 import ProfilePage from "./pages/profiles/ProfilePage";
+import UsernameForm from "./pages/profiles/UsernameForm";
 
 
 function App() {
@@ -55,6 +56,7 @@ function App() {
             <Route exact path="/posts/:id/edit" render={() => <PostEditForm /> } />
             <Route exact path="/posts/:id" render={() => <PostPage /> } />
             <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
+            <Route exact path="/profiles/:id/edit/username" render={() => <UsernameForm />} />
             <Route render={() => <p>Page Not Found!</p>} />
           </Switch>
         </Container>
