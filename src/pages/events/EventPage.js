33f79dca707cc3748b50e351304e37b2bec13936
Patt Höge <paty.hoge98@gmail.com>
2024-03-02 07:16:08 +0000

    fetching event data

--- src/pages/events/EventPage.js
+++ src/pages/events/EventPage.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosReq } from "../../api/axiosDefaults";
 
 
 
 function EventPage() {
-  
+  const  { id } = useParams();
+  const [event, setEvent] = useState({ results: [] });
+
+  useEffect(() => {
+    const handleMount = async () => {
+        try {
+            const [{ data: event }] = await Promise.all([
+                axiosReq.get(`/events/${id}`),
+            ]);
+            console.log(event)
+            setEvent({ results: [event] });
+        } catch (err) {
+            console.log(err);
+        };
+    };
+    handleMount();
+}, [id]);
 
   return (
     <Container>
