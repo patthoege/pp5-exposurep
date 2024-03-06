48b065c325a01c844211b5f2bac2bbbb83346504
Patt Höge <paty.hoge98@gmail.com>
2024-03-06 11:34:26 +0000

    added a 3d model script and modify layout css to signin page

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -6,14 +6,12 @@ import Alert from "react-bootstrap/Alert";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-import Image from "react-bootstrap/Image";
 import Container from "react-bootstrap/Container";
 
 import { Link, useHistory } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
-import appStyles from "../../App.module.css";
 import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
 import { useRedirect } from "../../hooks/useRedirect";
 
@@ -50,72 +48,69 @@ function SignInForm() {
       };
 
   return (
-    <Row className={styles.Row}>
+   <div className={styles.FullScreen}>
+      <spline-viewer url="https://prod.spline.design/UOR17FL-TIPiTqGW/scene.splinecode"></spline-viewer> 
+    <div className={`${styles.Row} ${styles.Overlay}`}>
       <Col className="my-auto p-0 p-md-2" md={6}>
-        <Container className={`${appStyles.Content} p-4 `}>
-          <h1 className={styles.Header}>Sign In</h1>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="username">
-                <Form.Label className="d-none">Username</Form.Label>
-                <Form.Control 
-                className={styles.Input}
-                type="text" 
-                placeholder="Username" 
-                name="username"
-                value={username}
-                onChange={handleChange}
-                />
-            </Form.Group>
-            {errors.username?.map((message, idx) => (
-                <Alert key={idx} variant="warning">
-                    {message}
-                </Alert>
-            ))}
+        <Container className="p-4">
+          <Row className="justify-content-center">
+              <Col xs={12} sm={10} md={8} lg={6}>
+                <h1 className={styles.Header}>Sign In</h1>
+                <Form onSubmit={handleSubmit}>
+                  <Form.Group controlId="username">
+                    <Form.Label className="d-none">Username</Form.Label>
+                      <Form.Control 
+                        className={styles.Input}
+                        type="text" 
+                        placeholder="Username" 
+                        name="username"
+                        value={username}
+                        onChange={handleChange}
+                      />
+                  </Form.Group>
+                  {errors.username?.map((message, idx) => (
+                      <Alert key={idx} variant="warning">
+                          {message}
+                      </Alert>
+                  ))}
 
-            <Form.Group controlId="formBasicPassword">
-                <Form.Label className="d-none">Password</Form.Label>
-                <Form.Control 
-                className={styles.Input}
-                type="password" 
-                placeholder="Password" 
-                name="password"
-                value={password}
-                onChange={handleChange}
-                />
-            </Form.Group>
-            {errors.password?.map((message, idx) => (
-              <Alert key={idx} variant="warning">
-                {message}
-              </Alert>
-            ))}
-            
-            <Button className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`} type="submit">
-                Sign In
-            </Button>
-            {errors.non_field_errors?.map((message, idx) => (
-                <Alert key={idx} variant="warning" className="mt-3">
-                    {message}
-                </Alert>
-            ))}
-        </Form>
-
-        </Container>
-        <Container className={`mt-3 ${appStyles.Content}`}>
-          <Link className={styles.Link} to="/signup">
-            Don't have an account? <span>Sign up now!</span>
-          </Link>
-        </Container>
-      </Col>
-      <Col
-        md={6}
-        className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
-      >
-        <Image
-          className={`${appStyles.FillerImage}`}
-          src={"https://img.freepik.com/free-photo/lens-film-camera_587448-5090.jpg?t=st=1708441789~exp=1708445389~hmac=51bfd6515e3d6a87ff5e7b6ad4ea5629c85498d2c8abc5b4f3d58eca1db0356b&w=1380"}
-        />
-      </Col>
-    </Row>
+                  <Form.Group controlId="formBasicPassword">
+                    <Form.Label className="d-none">Password</Form.Label>
+                    <Form.Control 
+                      className={styles.Input}
+                      type="password" 
+                      placeholder="Password" 
+                      name="password"
+                      value={password}
+                      onChange={handleChange}
+                    />
+                  </Form.Group>
+                  {errors.password?.map((message, idx) => (
+                    <Alert key={idx} variant="warning">
+                      {message}
+                    </Alert>
+                  ))}
+                
+                  <Button className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`} type="submit">
+                      Sign In
+                  </Button>
+                  {errors.non_field_errors?.map((message, idx) => (
+                      <Alert key={idx} variant="warning" className="mt-3">
+                          {message}
+                      </Alert>
+                  ))}
+                </Form>
+              </Col>
+            </Row>
+          </Container>
+          <Container className="mt-3">
+            <Link className={styles.Link} to="/signup">
+              Don't have an account? <span>Sign up now!</span>
+            </Link>
+          </Container>
+        </Col>
+      </div>
+    </div>
   );
 }
 
