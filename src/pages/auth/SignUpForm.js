c35b1f51de2b0fb76286fab01253b5aa4ce4bd41
Patt Höge <paty.hoge98@gmail.com>
2024-03-06 12:04:30 +0000

    added a 3d model script and modify layout css to signup page

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -3,9 +3,8 @@ import { Link, useHistory } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
-import appStyles from "../../App.module.css";
 
-import { Form, Button, Image, Col, Row, Container, Alert } from "react-bootstrap";
+import { Form, Button, Col, Row, Container, Alert } from "react-bootstrap";
 import axios from "axios";
 import { useRedirect } from "../../hooks/useRedirect";
 
@@ -41,90 +40,86 @@ const SignUpForm = () => {
     };
 
   return (
-    <Row className={styles.Row}>
-      <Col className="my-auto py-2 p-md-2" md={6}>
-        <Container className={`${appStyles.Content} p-4 `}>
-          <h1 className={styles.Header}>sign up</h1>
-          <Form onSubmit={handleSubmit}> 
-            <Form.Group controlId="username">
-                <Form.Label className="d-none">Username</Form.Label>
-                <Form.Control 
-                    className={styles.Input}
-                    type="text" 
-                    placeholder="Username" 
-                    name="username"
-                    value={username}
-                    onChange={handleChange}
-                />
-            </Form.Group>
-            {errors.username?.map((message, idx) => (
-              <Alert variant="warning" key={idx}>
-                {message}
-              </Alert>
-            ))}
-            
-            <Form.Group controlId="formBasicPassword1">
-                <Form.Label className="d-none">Password</Form.Label>
-                <Form.Control 
-                    className={styles.Input}
-                    type="password" 
-                    placeholder="Password" 
-                    name="password1" 
-                    value={password1}
-                    onChange={handleChange}
-                />
-            </Form.Group>
-            {errors.password1?.map((message, idx) => (
-              <Alert key={idx} variant="warning">
-                {message}
-              </Alert>
-            ))}
+    <div className={styles.FullScreen}>
+      <spline-viewer url="https://prod.spline.design/UOR17FL-TIPiTqGW/scene.splinecode"></spline-viewer> 
+      <div className={`${styles.Row} ${styles.Overlay}`}>
+        <Col className="my-auto p-0 p-md-2" md={6}>
+          <Container className="p-4">
+            <Row className="justify-content-center">
+                <Col xs={12} sm={10} md={8} lg={6}>
+                  <h1 className={styles.Header}>sign up</h1>
+                  <Form onSubmit={handleSubmit}> 
+                  <Form.Group controlId="username">
+                        <Form.Label className="d-none">Username</Form.Label>
+                        <Form.Control 
+                          className={styles.Input}
+                          type="text" 
+                          placeholder="Username" 
+                          name="username"
+                          value={username}
+                          onChange={handleChange}
+                        />
+                      </Form.Group>
+                      {errors.username?.map((message, idx) => (
+                        <Alert variant="warning" key={idx}>
+                          {message}
+                        </Alert>
+                      ))}
+                      
+                    <Form.Group controlId="formBasicPassword1">
+                      <Form.Label className="d-none">Password</Form.Label>
+                            <Form.Control 
+                        className={styles.Input}
+                        type="password" 
+                        placeholder="Password" 
+                        name="password1" 
+                        value={password1}
+                        onChange={handleChange}
+                      />
+                    </Form.Group>
+                    {errors.password1?.map((message, idx) => (
+                      <Alert key={idx} variant="warning">
+                        {message}
+                      </Alert>
+                    ))}
 
-            <Form.Group controlId="formBasicPassword2">
-                <Form.Label className="d-none">Confirm password</Form.Label>
-                <Form.Control 
-                    className={styles.Input}
-                    type="password" 
-                    placeholder="Confirm password" 
-                    name="password2" 
-                    value={password2}
-                    onChange={handleChange}
-                />
-            </Form.Group>
-            {errors.password2?.map((message, idx) => (
-              <Alert key={idx} variant="warning">
-                {message}
-              </Alert>
-            ))}
+                    <Form.Group controlId="formBasicPassword2">
+                      <Form.Label className="d-none">Confirm password</Form.Label>
+                      <Form.Control 
+                          className={styles.Input}
+                          type="password" 
+                          placeholder="Confirm password" 
+                          name="password2" 
+                          value={password2}
+                          onChange={handleChange}
+                      />
+                    </Form.Group>
+                    {errors.password2?.map((message, idx) => (
+                      <Alert key={idx} variant="warning">
+                        {message}
+                      </Alert>
+                    ))}
 
-            <Button className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`} type="submit">
-                Sign Up
-            </Button>
-            {errors.non_field_errors?.map((message, idx) => (
-              <Alert key={idx} variant="warning" className="mt-2">
-                {message}
-              </Alert>
-            ))}
-        </Form>
-        </Container>
-        <Container className={`mt-3 ${appStyles.Content}`}>
-          <Link className={styles.Link} to="/signin">
-            Already have an account? <span>Sign in</span>
-          </Link>
-        </Container>
-      </Col>
-      <Col
-        md={6}
-        className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
-      >
-        <Image
-          className={`${appStyles.FillerImage}`}
-          src={
-            "https://img.freepik.com/free-photo/vibrant-s-curves-lens-generated-by-ai_24640-82071.jpg?t=st=1708434733~exp=1708438333~hmac=d1a8d38d51dfb85e99bebef1fe3ad3ea871440dffb5fb69f4874d41133198c01&w=1380"
-          }
-        />
-      </Col>
-    </Row>
+                    <Button className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`} type="submit">
+                        Sign Up
+                    </Button>
+                    {errors.non_field_errors?.map((message, idx) => (
+                      <Alert key={idx} variant="warning" className="mt-2">
+                        {message}
+                      </Alert>
+                    ))}
+                  </Form> 
+                </Col>
+              </Row>
+           </Container>
+          <Container className="mt-3">
+            <Link className={styles.Link} to="/signin">
+              Already have an account? <span>Sign in</span>
+            </Link>
+          </Container>
+        </Col>
+      </div>
+    </div>
   );
 };
 
